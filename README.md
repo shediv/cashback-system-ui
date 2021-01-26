00a771f936f897e9c696d8976ddf6aedc1c2a442
videsh_hedge <videsh_hedge@persistent.com>
2021-01-26 20:43:15 +0530

    Added navbar

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 ## How to start
 
 ### `npm install inside the root folder and npm start to run the front-end`
+
+Note: If any error during installation related to eslint then please re-install react scripts under local directory
+
+After `npm start` app will start running on http://localhost:3000/
+
+There are 3 pages
+1. http://localhost:3000/ => Gets all cashbacks
+2. http://localhost:3000/transaction => Gets all transactions, user can also add new transaction and delete          transaction
+3. http://localhost:3000/ruleset => Gets all rulesets, user can also add new ruleset and delete ruleset
+
+
--- src/containers/header.tsx
+++ src/containers/header.tsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 const HeaderComponent = () => {
   return (
     <div>
-        <h5>Cashback System</h5>
-        <nav>            
-            <Link to="/">Home</Link>
-            <Link to="/transaction">Transaction</Link>
-            <Link to="/ruleset">Ruleset</Link>
-        </nav>
+        <h5 className="title">Cashback System</h5>
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/transaction">Transaction</Link></li>
+            <li><Link to="/ruleset">Ruleset</Link></li>
+        </ul>
       <hr />
     </div>
   );
